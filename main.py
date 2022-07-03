808e51594ba9abfc2c6d06cb786a8f479c60becd
Jake <91381193+MaundoJako@users.noreply.github.com>
2022-07-03 21:19:31 +0000

    update

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
-bill = 0
-if height >= 120:
-  print("You can ride the rollercoaster!")
-  age = int(input("What is your age? "))
-  if age < 12:
-    bill = 5
-    print("Child tickets are £5")
-  elif age <=18:
-    bill = 7
-    print("Youth tickets are £7")
-  else:
-    bill = 12
-    print("Adult tickets are £12")
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
 
-  wants_photo = input("Want Photos for £3? y or n ")
-  if wants_photo == "y":
-    bill += 3
-  print(f"Your total bill is £{bill}")
-  
+#Write your code below this line 👇
+
+bill = 0
+if size == "S":
+    bill += 15
+    if add_pepperoni == "Y":
+        bill += 2
+elif size == "M":
+    bill += 20
+    if add_pepperoni == "Y":
+        bill += 3
 else:
-  print("Sorry, you have to grow taller before you can ride.")
+    bill += 25
+    if add_pepperoni == "Y":
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1 
+print(f"Your final bill is: {bill}")
