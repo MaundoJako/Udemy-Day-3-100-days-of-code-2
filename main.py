8fcb7a32a9517584b7535d3de2c0d65151188519
Jake <91381193+MaundoJako@users.noreply.github.com>
2022-07-03 20:51:28 +0000

    added teacher version

--- main.py
+++ main.py
@@ -20,3 +20,16 @@ elif calc1 == 0:
     print("Leap year.")
 else:
     print("Not leap year.")
+
+#### teacher's version
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("leap year")
+    else:
+      print("not leap year")
+  else:
+    print("leap year")
+else:
+  print("not leap year")
